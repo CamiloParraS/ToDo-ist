dc638e6acd1bb0a73b9fd2bf6dff5c0b38327783
Camilo Parra <juanparracsfn@gmail.com>
2026-01-24 09:19:32 -0500

    made the project class, hope it works
    who builds a house starting by the windows?
    MEEE!!!!

---
+++ src/models/project.js
@@ -0,0 +1,39 @@
+import { toDate, isToday, isThisWeek, subDays } from "date-fns";
+
+export default class Project {
+  constructor(name) {
+    this.id = crypto.randomUUID();
+
+    this.name = name;
+    this._tasks = [];
+  }
+
+  // NAME
+  get name() {
+    return this._name;
+  }
+  set name(value) {
+    this._name = value;
+  }
+
+  // TASKS
+  get tasks() {
+    return this._tasks;
+  }
+  set tasks(tasks) {
+    this._tasks = tasks;
+  }
+
+  addTask(newTask) {
+    if (this._tasks.some((task) => task.id === newTask.id)) return;
+    this._tasks.push(newTask);
+  }
+
+  deleteTask(id) {
+    this._tasks = this._tasks.filter((task) => task.id !== id);
+  }
+
+  getTask(id) {
+    return this._tasks.find((task) => task.id === id);
+  }
+}
