0b405926f81b8cc33dd189cd558556d181249e98
Camilo Parra <juanparracsfn@gmail.com>
2026-01-25 10:57:10 -0500

    created the dom controller, needed to load elements, will be refactored later to be allocated in multiple modules

--- src/index.js
+++ src/index.js
@@ -1,4 +1,8 @@
 import "modern-normalize";
 import "./styles/main.css";
 
+import domController from "./ui/domController.js";
+
 console.log("heheh HI!");
+
+domController.loadMainContent();
---
+++ src/ui/domController.js
@@ -0,0 +1,63 @@
+import Project from "../models/project";
+import Todo from "../models/todo";
+import applogic from "../logic/applogic";
+
+const domController = {
+  loadMainContent() {
+    this.loadProjects();
+    this.loadTasks();
+  },
+
+  loadProjects() {
+    const projectsContainer = document.getElementById("projects-container");
+    projectsContainer.innerHTML = "";
+    applogic.projects.forEach((project) => {
+      if (project.name !== "root") {
+        this.createProject(project.name);
+      }
+    });
+  },
+
+  loadTasks() {
+    const tasksContainer = document.getElementById("tasks-container");
+    tasksContainer.innerHTML = "";
+    applogic.projects.forEach((project) => {
+      if (project.name !== "root" && project.tasks.length > 0) {
+        const projectSection = document.createElement("div");
+        projectSection.classList.add("section");
+        projectSection.innerHTML = `<div class="section-header">${project.name}</div>`;
+        project.tasks.forEach((task) => {
+          projectSection.innerHTML += this.createTask(task);
+        });
+        projectSection.innerHTML += `<div class="add-task-link">Add task</div>`;
+        tasksContainer.appendChild(projectSection);
+      }
+    });
+  },
+
+  createProject(name) {
+    const userProjects = document.getElementById("projects-container");
+    userProjects.innerHTML += `
+      <div class="project-item">
+        <span class="project-hash">#</span>
+          ${name}
+        </div>
+    `;
+  },
+
+  createTask(task) {
+    return `
+      <div class="task">
+          <div class="task-checkbox"></div>
+          <div class="task-content">
+              <div class="task-title">${task.title}</div>
+              <div class="task-meta">
+                  <span class="task-time">${task.dueDate}</span>
+              </div>
+          </div>
+      </div>
+    `;
+  },
+};
+
+export default domController;
